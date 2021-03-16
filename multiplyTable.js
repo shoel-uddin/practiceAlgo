35d414861732d6089b33a7bba3e0fac26a254c2e
Shoel Uddin <shoeluddin@Shoels-MBP.attlocal.net>
2021-03-16 18:05:16 -0400

    multi & small||large number

--- multiplyTable.js
+++ multiplyTable.js
@@ -5,4 +5,5 @@ for (let i = 0; i < count; i++) {
     i++;
     console.log(multiTable, "X", i, "=", multiTable * i);
     --i;
+    
 }
---
+++ smallestOrLargest.js
@@ -0,0 +1,10 @@
+const numbers = [2, 4, 9, 2, 0, 16, 24];
+
+const smallestNum = Math.min(...numbers);
+const largestNum = Math.max(...numbers);
+
+console.log('Smallest Value:', smallestNum); 
+console.log('Largest Value:', largestNum);   
+
+
+
